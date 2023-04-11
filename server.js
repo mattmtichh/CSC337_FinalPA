c63b3921aa0512c1e7fc4f249051fb35125bf442
carsonchapman5 <70231784+carsonchapman5@users.noreply.github.com>
2023-04-11 14:28:27 -0700

    Adding Skeleton for Handling various client requests

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const fs = require('fs');
 var GameSchema = new mongoose.Schema( {
     user: String,
     difficulty: String,
-    time: Number
+    time: Number,
+    gameboard: {
+        type: [[Number, Number, Boolean]],
+        required: true
+    }
 })
 var Games = mongoose.model('Games' , GameSchema);
 
@@ -28,6 +32,54 @@ const app = express();
 app.use(express.static('public_html'));
 app.use(express.json());
 // app.use('app/*/',authenticate);
+
+app.post('/app/create/game/:user', (req, res) => {
+
+    // TODO - Implement logic to create a new game for a user (Will have to change /app/ path to what 
+    // the authenticate function is using).
+
+});
+
+app.get('/app/get/games/:user', (req, res) => {
+
+    // TODO - Implement logic to get all games for a user (Will have to change /app/ path to what 
+    // the authenticate function is using).
+
+});
+
+app.get('/get/games', (req, res) => {
+
+    // TODO - Implement logic to get all games (Leaderboards can be accessed by anyone?)
+
+});
+
+app.get('/get/users', (req, res) => {
+
+    // TODO - Implement logic to get all users (User list can be accessed by anyone?)
+
+});
+
+app.post('/create/user', (req, res) => {
+
+    // TODO - Implement logic to create a new user (Stored in request body?)
+
+});
+
+app.post('/app/step/:user', (req, res) => {
+
+    // TODO - Implement logic to step through a game (Will have to change /app/ path to what 
+    // the authenticate function is using).
+
+
+});
+
+app.post('/app/flag/:user', (req, res) => {
+
+    // TODO - Implement logic to flag a space (Will have to change /app/ path to what 
+    // the authenticate function is using).
+
+});
+
 
 const port = 3000;
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
